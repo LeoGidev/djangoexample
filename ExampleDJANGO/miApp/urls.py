3b031eecea3a3576c32e5d47729afff491ba0fbb
Leo Gimenez <leogimenez.dev@gmail.com>
2024-11-05 15:32:15 -0300

    se trabaja en templates

--- ExampleDJANGO/miApp/urls.py
+++ ExampleDJANGO/miApp/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from .views import login_view, dashboard, logout_view
+from .views import crear_usuario
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('dashboard/', dashboard, name='dashboard'),
     path('logout/', logout_view, name='logout'),
-]
-
-from .views import crear_usuario
-urlpatterns += [
     path('crear_usuario/', crear_usuario, name='crear_usuario'),
 ]
+
+
