8d90fa5ae51b21bc3deffd106f7ebf17362bc94a
Leo Gimenez <leogimenez.dev@gmail.com>
2024-11-05 12:35:53 -0300

    se trabaja en modelos

--- ExampleDJANGO/miApp/models.py
+++ ExampleDJANGO/miApp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Dato(models.Model):
-    fecha = models.DateField()
-    id = models.AutoField(primary_key=True)
-    dato = models.CharField(max_length=255)
+    Fecha = models.DateField()
+    ID = models.AutoField(primary_key=True)
+    Dato = models.CharField(max_length=255)
     def __str__(self):
         return f"{self.fecha} - {self.dato}"
