dd10ebda54f1569de98ef03c564118e3357c987b
Leo Gimenez <leogimenez.dev@gmail.com>
2024-11-05 12:31:52 -0300

    se trabaja en modelos

--- ExampleDJANGO/miApp/models.py
+++ ExampleDJANGO/miApp/models.py
@@ -5,3 +5,5 @@ class Dato(models.Model):
     fecha = models.DateField()
     id = models.AutoField(primary_key=True)
     dato = models.CharField(max_length=255)
+    def __str__(self):
+        return f"{self.fecha} - {self.dato}"
