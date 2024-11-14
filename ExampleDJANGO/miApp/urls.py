1b84199dd377c4715f4b7509064728f0e3e0d1f8
Leo Gimenez <leogimenez.dev@gmail.com>
2024-11-14 10:38:13 -0300

    se trabaja en barra lateral

--- ExampleDJANGO/miApp/urls.py
+++ ExampleDJANGO/miApp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import login_view, dashboard, logout_view
 from .views import dash2
+from .views import login_view, dashboard, logout_view, dash2, agregar_dato
+
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('dashboard/', dashboard, name='dashboard'),
     path('logout/', logout_view, name='logout'),
     path('pagina2/', dash2, name='dashboard2'),
+    path('agregar-dato/', agregar_dato, name='agregar_dato'),  # Ruta para agregar_dato
 ]
+
 
 
