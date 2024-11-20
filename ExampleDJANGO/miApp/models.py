4072cebd0f7c36c19164223e44a9ac7bc3967610
Leo Gimenez <leogimenez.dev@gmail.com>
2024-11-20 11:19:17 -0300

    se trabaja en corregir modelo

--- ExampleDJANGO/miApp/models.py
+++ ExampleDJANGO/miApp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Dato(models.Model):
-    fecha = models.DateTimeField()  # Cambié a DateTimeField para que coincida con el uso de timezone.now() en la vista
+    fecha = models.DateTimeField(auto_now_add=True)   # Cambié a DateTimeField para que coincida con el uso de timezone.now() en la vista
     id = models.AutoField(primary_key=True)
     dato = models.CharField(max_length=255)
+    
 
     def __str__(self):
         return f"{self.fecha} - {self.dato}"
