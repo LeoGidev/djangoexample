8e9697b17c6de642eae4417a9ccbd6a8cced86d2
Leo Gimenez <leogimenez.dev@gmail.com>
2024-11-22 11:25:47 -0300

    se trabaja en mostrar datos

--- ExampleDJANGO/miApp/models.py
+++ ExampleDJANGO/miApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Dato(models.Model):
-    Fecha = models.DateTimeField(auto_now_add=True)   # Cambié a DateTimeField para que coincida con el uso de timezone.now() en la vista
+    Fecha = models.DateField(auto_now_add=True)   
     ID = models.AutoField(primary_key=True)
     Dato = models.CharField(max_length=255)
 
