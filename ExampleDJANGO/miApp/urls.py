1b4b4f20223b3227c028349ccb4eec907d7f4983
Leo Gimenez <leogimenez.dev@gmail.com>
2024-11-26 12:26:07 -0300

    se trabaja en pagina de perfil

--- ExampleDJANGO/miApp/urls.py
+++ ExampleDJANGO/miApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import dash2
-from .views import login_view, dashboard, logout_view, dash2, agregar_dato
+from .views import login_view, dashboard, logout_view, dash2, agregar_dato, perfil_usuario
 
 
 urlpatterns = [
@@ -9,7 +9,7 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('pagina2/', dash2, name='dashboard2'),
     path('agregar-dato/', agregar_dato, name='agregar_dato'),  # Ruta para agregar_dato
-    path('profile/', views.perfil_usuario, name='perfil_usuario'),
+    path('profile/', perfil_usuario, name='perfil_usuario'),
 ]
 
 
