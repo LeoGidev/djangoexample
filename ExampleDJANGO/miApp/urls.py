db9862bae4441419a31c4b11671fa47827d3b29f
Leo Gimenez <leogimenez.dev@gmail.com>
2024-11-26 13:31:10 -0300

    se trabaja en pagina de perfil

--- ExampleDJANGO/miApp/urls.py
+++ ExampleDJANGO/miApp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('pagina2/', dash2, name='dashboard2'),
     path('agregar-dato/', agregar_dato, name='agregar_dato'),  # Ruta para agregar_dato
-    path('profile/', perfil_usuario, name='perfil_usuario'),
+    path('profile/', perfil_usuario, name='profile'),
 ]
 
 
