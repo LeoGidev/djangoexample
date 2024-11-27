076a7d17999efb96ef920067447b868093393a7d
Leo Gimenez <leogimenez.dev@gmail.com>
2024-11-27 15:34:38 -0300

    se trabaja en modelo para perfil

--- ExampleDJANGO/miApp/form.py
+++ ExampleDJANGO/miApp/form.py
@@ -7,3 +7,5 @@ class PerfilForm(forms.ModelForm):
         fields = ['foto', 'descripcion', 'cumpleaños', 'telefono', 'direccion']
         widgets = {
             'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'cumpleaños': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'telefono': forms.TextInput(attrs={'class': 'form-control'}),
