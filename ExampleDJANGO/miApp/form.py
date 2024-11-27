85e774fe7ee3121d552f04492159a2f8e5b91dc6
Leo Gimenez <leogimenez.dev@gmail.com>
2024-11-27 15:33:56 -0300

    se trabaja en modelo para perfil

--- ExampleDJANGO/miApp/form.py
+++ ExampleDJANGO/miApp/form.py
@@ -4,3 +4,5 @@ from .models import Perfil
 class PerfilForm(forms.ModelForm):
     class Meta:
         model = Perfil
+        fields = ['foto', 'descripcion', 'cumpleaños', 'telefono', 'direccion']
+        widgets = {
