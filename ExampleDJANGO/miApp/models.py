bd9b270fb7475b4daaee7378961dc06140502037
Leo Gimenez <leogimenez.dev@gmail.com>
2024-11-27 15:21:36 -0300

    se trabaja en modelo para perfil

--- ExampleDJANGO/miApp/models.py
+++ ExampleDJANGO/miApp/models.py
@@ -15,4 +15,5 @@ class Dato(models.Model):
 
 class Perfil(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
-    
+    foto = models.ImageField(upload_to='fotos_perfil/', blank=True, null=True)
+      
